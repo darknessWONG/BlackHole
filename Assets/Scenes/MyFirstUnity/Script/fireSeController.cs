48268dc6f5b368cab0dcc6d8ea37147a26d234c7
zz <2017ablestzz@gmail.com>
2019-04-25 12:34:12 +0900

    add scripts

---
+++ Assets/Scenes/MyFirstUnity/Script/fireSeController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireSeController : MonoBehaviour
+{
+
+    private AudioSource m_fire;
+    private bool m_isPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_fire = gameObject.GetComponent<AudioSource>();
+        m_isPlay = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<playerController>();
+        // 距離計算
+        float disTemp = Vector3.Distance(transform.position, pc.GetComponent<playerController>().transform.position);
+        if (!m_isPlay)
+        {
+            // チェック範囲内に入ると
+            if (disTemp < 12.5f)
+            {
+                /*===============   音の処理   ===============*/
+                //Debug.Log("fuck fire");
+                m_isPlay = true;
+                m_fire.Play();
+            }
+        }
+        else
+        {
+            // チェック範囲以外と
+            if (disTemp >= 12.5f)
+            {
+                /*===============   音の処理   ===============*/
+                //Debug.Log("fuck fire");
+                m_isPlay = false;
+                m_fire.Stop();
+            }
+        }
+
+    }
+}
---
+++ Assets/Scenes/MyFirstUnity/Script/seController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seController : MonoBehaviour
+{
+    private enum E_SeLIST
+    {
+        run,
+        absorption,
+        max,
+    }
+
+    public struct S_SE
+    {
+        public AudioSource se;
+        public bool isPlay;
+    }
+
+    private S_SE m_run;
+    private S_SE m_absorption;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_run.se = gameObject.GetComponents<AudioSource>()[(int)E_SeLIST.run];
+        m_absorption.se = gameObject.GetComponents<AudioSource>()[(int)E_SeLIST.absorption];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // BHのSEチェック
+        if(!m_absorption.isPlay)
+        {
+            playerController.E_BlackHoleStatus bhStatus =
+                GameObject.FindGameObjectWithTag("Player").GetComponent<playerController>().status;
+            if (bhStatus == playerController.E_BlackHoleStatus.E_BlackHoleOn)
+            {
+                m_absorption.isPlay = true;
+                m_absorption.se.Play();
+            }
+        }
+        else
+        {
+            playerController.E_BlackHoleStatus bhStatus =
+                GameObject.FindGameObjectWithTag("Player").GetComponent<playerController>().status;
+            if (bhStatus == playerController.E_BlackHoleStatus.E_BlackHoleOff)
+            {
+                m_absorption.isPlay = false;
+                m_absorption.se.Stop();
+            }
+        }
+
+        // 足音を止めるチェック
+        if (Input.GetKeyUp(KeyCode.A)
+            || Input.GetKeyUp(KeyCode.D))
+        {
+            if (m_run.isPlay)
+            {
+                m_run.isPlay = false;
+                m_run.se.Stop();
+            }
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A)
+            || Input.GetKey(KeyCode.D))
+        {
+            if(!m_run.isPlay)
+            {
+                m_run.isPlay = true;
+                m_run.se.Play();
+            }
+        }
+
+
+    }
+
+}
